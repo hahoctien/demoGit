2d79fd6a5f042b3a9d49fdaf13fac79096726ac6
HaiDo <dobahai007@gmail.com>
2020-04-12 15:58:14 +0700

    update main.js

---
+++ main.js
@@ -0,0 +1,15 @@
+console.log('Demo git')
+/**
+ * Khởi tạo git mới: git init
+ * Khai báo họ tên: git --global user.name "..."
+ * Khai báo email: git --global user.email ...
+ * Khai báo địa chỉ với server: git remote add origin ...
+ * Commit filt : git commit -m "..."
+ * Đẩy file lên gitHub : git push -u origin master
+ * 
+ * ==============================================================
+ * 
+ * Developer khác:
+ * Lấy folder từ gitHub: git clone "<link>""
+ * Trỏ đến folder trong máy tính: git cd "<link folder>""
+ */
